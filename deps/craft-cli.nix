15e496639d7d72e06c17e53053b7737a9fcbca56
Jon Seager <jon@sgrs.uk>
2023-08-30 10:08:34 +0100

    chore: craft-cli 1.2.0 -> 2.1.0

--- deps/craft-cli.nix
+++ deps/craft-cli.nix
@@ -4,23 +4,36 @@
 }:
 let
   pname = "craft-cli";
-  version = "1.2.0";
+  version = "2.1.0";
 in
 pkgs.python3Packages.buildPythonPackage rec {
   inherit pname version;
+  format = "pyproject";
 
   src = pkgs.fetchFromGitHub {
     owner = "canonical";
     repo = pname;
     rev = version;
-    sha256 = "sha256-kNaAvuZarAq/qo7g9htd0Y65SQ/zjrbKmDSAfAj+ydw=";
+    sha256 = "sha256-K9uJqegMFtEPssBIpYR3Ere11/war+uIa06GvkKTuxU=";
   };
+
+  postPatch = ''
+    substituteInPlace craft_cli/__init__.py \
+      --replace "dev" "${version}"
+    
+    substituteInPlace pyproject.toml \
+      --replace "setuptools==67.7.2" "setuptools"
+  '';
 
   propagatedBuildInputs = with pkgs.python3Packages; [
     platformdirs
     pydantic
     pyyaml
+    setuptools
+    setuptools-scm
   ];
+
+  SETUPTOOLS_SCM_PRETEND_VERSION = version;
 
   doCheck = false;
 
