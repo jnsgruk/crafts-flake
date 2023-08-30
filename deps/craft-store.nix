7d26e746903c5eacc8d0fda8f43a725ac1f83506
Jon Seager <jon@sgrs.uk>
2023-08-30 10:05:31 +0100

    chore: craft-store d1685167 -> 721955c9

--- deps/craft-store.nix
+++ deps/craft-store.nix
@@ -4,18 +4,24 @@
 }:
 let
   pname = "craft-store";
-  # Version 2.4.0 - no tag was created for this release
-  version = "d16851676d6ff632d063ed5e6199dc0d8aca93c7";
+  # Version 2.4.0 but version was not tagged
+  version = "2.4.0";
 in
 pkgs.python3Packages.buildPythonPackage rec {
   inherit pname version;
+  format = "pyproject";
 
   src = pkgs.fetchFromGitHub {
     owner = "canonical";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-i/d4EzrpFbv51z+qkCse7te0fIlotdBbGIjFsQwmzcw=";
+    rev = "721955c98fc5d991ab1233826d67faeed6f8c65d";
+    sha256 = "sha256-wNI3BVfSGWKV1hrMZ7hGNGksdHjCfv+vMLuoDh2f7M0=";
   };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace "setuptools==67.7.2" "setuptools"
+  '';
 
   propagatedBuildInputs = with pkgs.python3Packages; [
     keyring
@@ -24,7 +30,11 @@ pkgs.python3Packages.buildPythonPackage rec {
     pydantic
     requests
     requests-toolbelt
+    setuptools
+    setuptools-scm
   ];
+
+  SETUPTOOLS_SCM_PRETEND_VERSION = version;
 
   doCheck = false;
 
