b9a7aa17bb5e1b8067156823ef40e27ed0fc49c0
Jon Seager <jon@sgrs.uk>
2023-08-30 10:08:59 +0100

    fix: ensure pydantic-yaml parses version correctly during build

--- deps/pydantic-yaml.nix
+++ deps/pydantic-yaml.nix
@@ -8,11 +8,20 @@ let
 in
 pkgs.python3Packages.buildPythonPackage rec {
   inherit pname version;
+  format = "pyproject";
 
   src = pkgs.python3Packages.fetchPypi {
     inherit pname version;
     sha256 = "sha256-GcjzyalwQbCj2PwGylFD/3HAhGxFs5/ecZz7yYvnoAw=";
   };
+
+  postPatch = ''
+    substituteInPlace src/pydantic_yaml/__init__.py \
+      --replace "0.0.0" "${version}"
+    
+    substituteInPlace pyproject.toml \
+      --replace "setuptools==67.7.2" "setuptools"
+  '';
 
   propagatedBuildInputs = with pkgs.python3Packages; [
     deprecated
@@ -20,7 +29,11 @@ pkgs.python3Packages.buildPythonPackage rec {
     pydantic
     semver
     types-deprecated
+    setuptools
+    setuptools-scm
   ];
+
+  SETUPTOOLS_SCM_PRETEND_VERSION = version;
 
   doCheck = false;
 
