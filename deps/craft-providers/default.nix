38dee2714fd3a973f1bb151336099d111dff75cc
Jon Seager <jon@sgrs.uk>
2023-08-30 10:06:09 +0100

    chore: craft-providers 1.11.0 -> 1.15.0

--- deps/craft-providers/default.nix
+++ deps/craft-providers/default.nix
@@ -4,29 +4,42 @@
 }:
 let
   pname = "craft-providers";
-  version = "1.11.0";
+  version = "1.15.0";
 in
 pkgs.python3Packages.buildPythonPackage rec {
   inherit pname version;
+  format = "pyproject";
 
   src = pkgs.fetchFromGitHub {
     owner = "canonical";
     repo = pname;
     rev = version;
-    sha256 = "sha256-ORwwHEYeqf50vwriGpGFQNl2XJQGITLoPu/ddoM02Jw=";
+    sha256 = "sha256-Yy3ZINItHi+/xqot8BesaGEkqFPKgTiK3vf8gKN2McA=";
   };
 
   patches = [
     ./lxd-socket-path.patch
-    ./no-inject-snap.patch
+    ./python-submodules.patch
   ];
+
+  postPatch = ''
+    substituteInPlace craft_providers/__init__.py \
+      --replace "dev" "${version}"
+    
+    substituteInPlace pyproject.toml \
+      --replace "setuptools==67.7.2" "setuptools"
+  '';
 
   propagatedBuildInputs = with pkgs.python3Packages; [
     pydantic
     pyyaml
     requests
     requests-unixsocket
+    setuptools
+    setuptools-scm
   ];
+
+  SETUPTOOLS_SCM_PRETEND_VERSION = version;
 
   doCheck = false;
 
