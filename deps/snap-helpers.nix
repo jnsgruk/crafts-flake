9a970e39ec3a4e1d288f0bc00336f962bfad0c74
Jon Seager <jon@sgrs.uk>
2023-10-25 10:58:49 +0100

    chore: snap-helpers 0.3.2 -> 0.4.2

--- deps/snap-helpers.nix
+++ deps/snap-helpers.nix
@@ -4,19 +4,21 @@
 }:
 let
   pname = "snap-helpers";
-  version = "0.3.2";
+  version = "0.4.2";
 in
 pkgs.python3Packages.buildPythonPackage rec {
   inherit pname version;
+  format = "pyproject";
 
   src = pkgs.python3Packages.fetchPypi {
     inherit pname version;
-    sha256 = "sha256-Qd5qOAh71KPkbb1bvdBMWwiLnE3BuDWxVxWu1QOwhfk=";
+    sha256 = "sha256-7zuGIeMxu3Gv4n5U73QqfdLt2egCavrChb60IQnIuak=";
   };
 
   propagatedBuildInputs = with pkgs.python3Packages; [
     packaging
     pyyaml
+    setuptools
   ];
 
   doCheck = false;
