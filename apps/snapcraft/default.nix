4a70ea455c0e899f06a0e39d7918a686791d6703
Jon Seager <jon@sgrs.uk>
2024-01-15 20:53:58 +0000

    test: enable tests on snapcraft package

--- apps/snapcraft/default.nix
+++ apps/snapcraft/default.nix
@@ -17,8 +17,10 @@ pkgs.python3Packages.buildPythonApplication {
   };
 
   patches = [
+    ./os-platform.patch
     ./set-channel-for-nix.patch
     ./snapcraft-data-dirs.patch
+    ./tests-patch.patch
   ];
 
   postPatch = ''
@@ -35,6 +37,13 @@ pkgs.python3Packages.buildPythonApplication {
     substituteInPlace \
       snapcraft_legacy/__init__.py \
       --replace '__version__ = _get_version()' '__version__ = "${version}"'
+
+    substituteInPlace snapcraft/elf/elf_utils.py \
+      --replace 'linker_path = root_path / arch_config.dynamic_linker' \
+                'linker_path = Path("${pkgs.glibc}/lib/ld-linux-x86-64.so.2")'
+      
+    substituteInPlace tests/unit/linters/test_classic_linter.py \
+      --replace '"/lib/x86_64-linux-gnu/libdl.so.2"' '"${pkgs.glibc}/lib/libdl.so.2"'
   '';
 
   buildInputs = with pkgs; [ makeWrapper ];
@@ -74,8 +83,44 @@ pkgs.python3Packages.buildPythonApplication {
     wrapProgram $out/bin/snapcraft --prefix PATH : ${pkgs.squashfsTools}/bin
   '';
 
-  # TODO: Try to make the tests pass and remove this.
-  doCheck = false;
+  preCheck = ''
+    mkdir -p check-phase
+    export HOME=$(pwd)/check-phase
+  '';
+
+  nativeCheckInputs = with pkgs.python3Packages; [
+    pytest-check
+    pytest-cov
+    pytest-mock
+    pytest-subprocess
+    pytestCheckHook
+  ] ++ (with pkgs; [
+    git
+    squashfsTools
+  ]);
+
+  disabledTestPaths = [
+    "tests/legacy/unit"
+    "tests/unit/elf"
+  ];
+
+  disabledTests = [
+    "test_bin_echo"
+    "test_classic_linter_filter"
+    "test_classic_linter"
+    "test_get_base_configuration_snap_channel"
+    "test_get_base_configuration_snap_instance_name_default"
+    "test_get_base_configuration_snap_instance_name_not_running_as_snap"
+    "test_get_extensions_data_dir"
+    "test_get_os_platform_alternative_formats"
+    "test_get_os_platform_linux"
+    "test_get_os_platform_windows"
+    "test_patch_elf"
+    "test_remote_builder_init"
+    "test_setup_assets_remote_icon"
+    "test_validate_architectures_supported"
+    "test_validate_architectures_unsupported"
+  ];
 
   meta = {
     description = "Package, distribute, and update any app for Linux and IoT.";
